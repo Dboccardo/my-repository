98e72818b8e8105069283248476b5f7cab2336d2
Mosh Hamedani <moshfegh@live.com.au>
2017-06-12 12:03:21 -0700

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthorsService } from './authors.service';
 import { CoursesService } from './courses.service';
 import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,18 +6,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
-    CoursesComponent
+    CoursesComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [
-    CoursesService
+    CoursesService,
+    AuthorsService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,22 +1,16 @@
-import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
     template: `
         <h2>{{ title }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+        <h2 [textContent]="title"></h2>
+        
+        <img src="{{ imageUrl }}" />
+        <img [src]="title" />
     `
 })
 export class CoursesComponent {
     title = "List of courses";
-    courses;
-
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-    }
+    imageUrl = "http://lorempixel.com/400/200";
 }
