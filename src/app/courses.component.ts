fa9f605bf686a267d73210b14a445d021747f351
Mosh Hamedani <moshfegh@live.com.au>
2017-06-13 11:43:48 -0700

    .

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,13 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-<input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+    
     `
 })
 export class CoursesComponent {
-    email = "me@example.com";
-
-    onKeyUp() {
-        console.log(this.email);
-    }
 }
