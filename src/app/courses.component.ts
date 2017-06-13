ecd511b9cfd73b573ed30d64dd31f514c121d300
Mosh Hamedani <moshfegh@live.com.au>
2017-06-13 10:10:16 -0700

    .

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,14 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ title }}</h2>
-        <h2 [textContent]="title"></h2>
-        
-        <img src="{{ imageUrl }}" />
-        <img [src]="title" />
+        <input #email (keyup.enter)="onKeyUp(email.value)" />
     `
 })
 export class CoursesComponent {
-    title = "List of courses";
-    imageUrl = "http://lorempixel.com/400/200";
+    onKeyUp(email) {
+        console.log(email);
+    }
 }
