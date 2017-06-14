32e1fa684cc7086b21165fd8fbf18ea15df6eff1
Mosh Hamedani <moshfegh@live.com.au>
2017-06-14 12:41:20 -0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,7 +12,7 @@ export class AppComponent {
     isFavorite: true
   }
 
-  onFavoriteChanged() {
-    console.log("Favorite changed");
+  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
+    console.log("Favorite changed: ", eventArgs);
   }
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -16,6 +16,10 @@ export class FavoriteComponent implements OnInit {
 
   onClick() { 
     this.isSelected = !this.isSelected;
-    this.change.emit();
+    this.change.emit({ newValue: this.isSelected });
   }
+}
+
+export interface FavoriteChangedEventArgs {
+  newValue: boolean
 }
