82dbd9fd74e632835f3530da602305b16d0d944e
Mosh Hamedani <moshfegh@live.com.au>
2017-06-14 12:55:05 -0700

    .

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
   @Input('isFavorite') isSelected: boolean;
-  @Output() change = new EventEmitter();
+  @Output('change') click = new EventEmitter();
 
   constructor() { }
 
@@ -16,7 +16,7 @@ export class FavoriteComponent implements OnInit {
 
   onClick() { 
     this.isSelected = !this.isSelected;
-    this.change.emit({ newValue: this.isSelected });
+    this.click.emit({ newValue: this.isSelected });
   }
 }
 
