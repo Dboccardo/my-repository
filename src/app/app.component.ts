60bf5a74c53418a244ba251bc57bd224f2de02f5
Mosh Hamedani <moshfegh@live.com.au>
2017-06-21 14:53:34 -0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string;
+  tweet = { 
+    body: '...',
+    likesCount: 10,
+    isLiked: true
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { PanelComponent } from './panel/panel.component';
 import { InputFormatDirective } from './input-format.directive';
 import { TitleCasePipe } from './title-case.pipe';
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { TitleCasePipe } from './title-case.pipe';
     PanelComponent,
     InputFormatDirective,
     TitleCasePipe,
+    LikeComponent,
   ],
   imports: [
     BrowserModule,
