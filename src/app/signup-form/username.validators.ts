e442574c520a03bd51169d14c5a77fddd3476e81
Mosh Hamedani <moshfegh@live.com.au>
2017-06-27 12:43:05 -0700

    .

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -8,13 +8,14 @@ export class UsernameValidators {
         return null;
     }
     
-    static shouldBeUnique(control: AbstractControl) : ValidationErrors | null {
-        setTimeout(() => {
-            if (control.value === 'mosh')        
-                return { shouldBeUnique: true };
-            return null;
-        }, 2000);
-
-        return null;
+    static shouldBeUnique(control: AbstractControl) : Promise<ValidationErrors | null> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === 'mosh')        
+                    resolve({ shouldBeUnique: true });
+                else 
+                    resolve(null);
+            }, 2000);
+        });
     }
 }
