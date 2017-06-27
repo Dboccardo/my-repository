163a46bf107f5e3cb9851888393ddf63de72fd72
Mosh Hamedani <moshfegh@live.com.au>
2017-06-27 10:32:43 -0700

    .

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'signup-form',
@@ -8,7 +8,11 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
+
+  get username() {
+    return this.form.get('username');
+  }
 }
