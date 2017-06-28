316802b702a978362cfaf01a72cc2defd277dc5b
Mosh Hamedani <moshfegh@live.com.au>
2017-06-28 10:03:58 -0700

    .

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent {
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
 
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  removeTopic(topic: FormControl) {
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
 }
