aa5a538529e3a07dc784650ecfe3295943b5339c
Mosh Hamedani <moshfegh@live.com.au>
2017-06-29 11:00:12 -0700

    .

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -34,4 +34,12 @@ export class PostsComponent {
         console.log(response.json());
       })
   }
+
+  deletePost(post) {
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      });
+  }
 }
