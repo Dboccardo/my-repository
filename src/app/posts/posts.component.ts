3ee809290893fd36342dd6f63c63c1456331d03f
Mosh Hamedani <moshfegh@live.com.au>
2017-06-29 09:44:47 -0700

    .

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
-
+export class PostsComponent {
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
